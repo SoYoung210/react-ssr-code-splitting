c716f33ea4d2cc32fdc26d250be9a436608d4ea5
SoYoung210 <ethdud1@gmail.com>
2019-10-14 12:06:37 +0900

    Delete useless comment

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import loadable from '@loadable/component';
 import { Provider } from 'react-redux';
 import store from '@/store/stores'
-// import User from './User';
-// import Org from './Org';
+
 const User = loadable(() => import(/* webpackChunkName: "user"*/ './User'))
 const Org = loadable(() => import(/* webpackChunkName: "org"*/ './Org'))
 
