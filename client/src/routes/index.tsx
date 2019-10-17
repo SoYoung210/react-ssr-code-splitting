30eb81973408bcd08236a11698be06326f6cf8be
SoYoung210 <ethdud1@gmail.com>
2019-10-17 13:44:45 +0900

    fix: correct indent

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,18 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import loadable from '@loadable/component';
 import { Provider } from 'react-redux';
 import store from '@/store/stores'
-
 const User = loadable(() => import(/* webpackChunkName: "user"*/ './User'))
 const Org = loadable(() => import(/* webpackChunkName: "org"*/ './Org'))
 
 const EntryRoute: React.FC = () => {
   return (
-      <Provider store={store}>
-        <Switch>
-          <Route path='/user' component={User} />
-          <Route path='/org' component={Org} />
-        </Switch>
-      </Provider>
+    <Provider store={ store }>
+    <Switch>
+      <Route path='/user' component={User} />
+      <Route path='/org' component={Org} />
+    </Switch>
+    </Provider>
   );
 }
 
