54c805f93f5ec80c012717843b2023b1e60cb095
SoYoung210 <ethdud1@gmail.com>
2019-10-30 18:26:25 +0900

    Change EntryRoute export to default

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import loadable from '@loadable/component';
 import { Provider } from 'react-redux';
-import store from '@/store/stores'
-const User = loadable(() => import(/* webpackChunkName: "user"*/ './User'))
+import store from '@/store/stores';
+
+const User = loadable(() => import(/* webpackChunkName: "user"*/ './User'), {ssr: false})
 const Org = loadable(() => import(/* webpackChunkName: "org"*/ './Org'))
 
-const EntryRoute: React.FC = () => {
+export default () => {
   return (
     <Provider store={ store }>
-    <Switch>
-      <Route path='/user' component={User} />
-      <Route path='/org' component={Org} />
-    </Switch>
+      <Switch>
+        <Route path='/user' component={User} />
+        <Route path='/org' component={Org} />
+      </Switch>
     </Provider>
   );
 }
-
-export default EntryRoute;
