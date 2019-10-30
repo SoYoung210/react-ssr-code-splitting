976798c0b5a0e8a0c0dba8aad008ecb0e1f44c8a
SoYoung210 <ethdud1@gmail.com>
2019-10-30 16:46:24 +0900

    Add tsconfig

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
+import { loadableReady } from '@loadable/component';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import './normalize.pcss';
 import EntryRoute from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <EntryRoute />
-  </BrowserRouter>
-  , document.getElementById('root'));
-
+loadableReady(() => {
+  const root = document.getElementById('root');
+  hydrate(
+   
+      <BrowserRouter>
+        <EntryRoute />
+      </BrowserRouter>
+    ,
+    root
+  );
+});
